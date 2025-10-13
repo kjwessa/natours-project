2cbdefda89f5e5f475565bcc95da56cf0ac439a6
Kevin Wessa <39201312+kjwessa@users.noreply.github.com>
2025-10-13 11:12:55 -0400

    chore (biome): lint the project

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,20 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  root: '.',
-  server: {
-    open: true
-  },
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: './index.html'
-      }
-    }
-  },
-  css: {
-    postcss: './postcss.config.js'
-  }
+	root: ".",
+	server: {
+		open: true,
+	},
+	build: {
+		outDir: "dist",
+		emptyOutDir: true,
+		rollupOptions: {
+			input: {
+				main: "./index.html",
+			},
+		},
+	},
+	css: {
+		postcss: "./postcss.config.js",
+	},
 });
